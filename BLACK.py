6a95e5333319691a91761eabd957ab48d857599e
SokAlike <130259965+SokAlike@users.noreply.github.com>
2023-06-15 12:16:34 +0700

    Add files via upload

--- BLACK.py
+++ BLACK.py
@@ -1,9 +1,4 @@
 import os,platform
 os.system('git pull')
- 
-BLACK=platform.architecture()[0]
-if trt=="32bit":
-    print('Sorry 32 Bit Not Supported...')
-elif BLACK=="64bit":
-    __import__("BLACK")
+__import__("BLACK")
  
